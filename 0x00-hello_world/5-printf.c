30fd3f0bcfcb10f5a1813c16ed7b22da14bcf7f0
azeadrr <azeadn1@gmail.com>
2023-02-16 16:27:20 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,15 @@
-#!/bin/bash
 #include <stdio.h>
+
+
 /**
- * main - A program that print a line using the print function
- * Return: 0 (Success)
+ * main - Prints "with proper grammar, but the outcome is
+ * a piece of art," followed by a new line.
+ * Return: Always 0.
  */
+
 int main(void)
+
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
-return (0);
+            printf("with proper grammar, but the outcome is a piece of art,\n");
+            return (0);
 }
