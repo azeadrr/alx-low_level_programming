44df3da27b3e4ecf5884f4aadf93d68f9922d370
azeadrr <azeadn1@gmail.com>
2023-02-16 17:33:43 -0800

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
- * main - prints "Programming is like building a multilingual 
- * puzzle," followed by a newline.
+ * main - Entry
  * Return: Always 0.
  */
 int main(void)
