ae4ba6725fe27daa3b0eb8bb5e148685716d4c5a
azeadrr <azeadn1@gmail.com>
2023-02-21 15:48:57 -0800

    task13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,9 +9,9 @@ int main(void)
 		for (i = 0; i < 1024; i++)
 		{
 			if ((i % 3) == 0 || (i % 5) == 0)
-				sum += i;
+				z += i;
 		}
-		printf("%d\n", sum);
+		printf("%d\n", z);
 		return (0);
 }
 	
