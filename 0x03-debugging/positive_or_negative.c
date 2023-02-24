e0af75ec6dd0c5938bebb71aed8d8b566f889fa6
azeadrr <azeadn1@gmail.com>
2023-02-23 17:52:24 -0800

    task0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *    positive_or_negative - print a number
+ *    @i : integer
+ *    Description: if the number is greater than 0: is positive
+ *    if the number is 0: is zero
+ *     if the number is less than 0: is negative
+ *Return: 0
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is zero\n", i);
+}
