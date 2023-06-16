f7ecc3868d7ebf096dd322bf6fcf099e394504e4
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-06-15 17:40:32 -0700

    updated

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - Calculate function for finding the sum of data in a linked list
+ * @head:pointer head of linked list
+ * Return: if the list is empty, return 0
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int re = 0;
+
+	if (head == NULL)
+		return (0);
+
+	while (head->prev != NULL)
+		head = head->prev;
+
+	while (head != NULL)
+	{
+		re += head->n;
+		head = head->next;
+	}
+
+	return (re);
+}
