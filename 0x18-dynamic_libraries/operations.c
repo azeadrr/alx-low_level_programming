694008475ca29617fb34a672054c8806d3e99a0c
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-06-19 19:57:23 -0700

    updated

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,65 @@
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+/**
+* add - adds of two numbers
+* @a: first number to add
+* @b: second number to add
+* Return: result of two numbers with sum operation
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* sub - subtracts of two numbers
+* @a: first number to add
+* @b: second number to add
+* Return: result of two numbers with subtract operation
+*/
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* mul - multiplies of two numbers
+* @a: first number to add
+* @b: second number to add
+* Return: result of two numbers with  multiple operation
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* div - divideds of two numbers
+* @a: first number to add
+* @b: second number to add
+* Return: result of two numbers with div operation
+*/
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - the modulus of two numbers
+ * @a: first number to add
+ * @b: second number to add
+ * Return: result of two numbers with modulu operation
+*/
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
