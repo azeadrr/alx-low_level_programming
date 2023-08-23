91bd95d7aa4ddee0fb88c9172e13f6877babe494
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-08-23 16:01:59 -0700

    mandatory

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,16 +2,16 @@
 """ function def island_perimeter(grid): that returns the perimeter of the island described in grid"""
 
 def island_perimeter(grid):
-num = 0
-for row in range(len(grid)):
-for column in range(len(grid[row])):
-if (grid[row][column] == 1):
-if (column == len(grid[row]) - 1 or grid[row][column + 1] == 0):
-num = num + 1
-if (row == len(grid) - 1 or grid[row + 1][column] == 0):
-num = num + 1
-if (column == 0 or grid[row][column - 1] == 0):
-num = num + 1
-if (row == 0 or grid[row - 1][column] == 0):
-num = num + 1
-return (num)
+    num = 0
+    for row in range(len(grid)):
+        for column in range(len(grid[row])):
+            if (grid[row][column] == 1):
+                if (column == len(grid[row]) - 1 or grid[row][column + 1] == 0):
+                    num = num + 1
+                if (row == len(grid) - 1 or grid[row + 1][column] == 0):
+                    num = num + 1
+                if (column == 0 or grid[row][column - 1] == 0):
+                    num = num + 1
+                if (row == 0 or grid[row - 1][column] == 0):
+                    num = num + 1
+        return (num)
