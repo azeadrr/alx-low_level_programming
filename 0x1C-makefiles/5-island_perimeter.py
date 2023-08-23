b8fde70f9f9885272ff943c48fa1c84333a91042
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-08-23 15:56:01 -0700

    mandatory

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+""" function def island_perimeter(grid): that returns the perimeter of the island described in grid"""
+
 def island_perimeter(grid):
 num = 0
 for row in range(len(grid)):
