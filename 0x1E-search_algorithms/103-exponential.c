d897d4fde4d4bc0ae94ddf6b645cb779da36471b
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-10-10 15:59:36 -0700

    advanced tasks

---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,58 @@
+#include "search_algos.h"
+
+/**
+ * _binary_search - searches for a value in a sorted array of integers
+ * @array: is a pointer to the first element of the array to search in
+ * @size: is the number of elements in array
+ * @value: is the value to search for
+ * Return: If value is not present in array or if array is NULL
+ * your function must return -1
+ */
+int _binary_search(int *array, size_t left, size_t right, int value)
+{
+	size_t x;
+
+	if (array == NULL)
+		return (-1);
+	while (right >= left)
+	{
+		printf("Searching in array: ");
+		for (x = left; x < right; x++)
+			printf("%d, ", array[x]);
+		printf("%d\n", array[x]);
+
+		x = left + (right - left) / 2;
+		if (array[x] == value)
+			return (x);
+		if (array[x] > value)
+			right = x - 1;
+		else
+			left = x + 1;
+	}
+	return (-1);
+}
+
+/**
+ * exponential_search - Searches for a value in a sorted array
+ * @array: is a pointer to the first element of the array to search
+ * @size: is the number of elements in array
+ * @value: is the value to search
+ * Return: If value is not present in array or if array is NULL
+ * your function must return -1
+ */
+int exponential_search(int *array, size_t size, int value)
+{
+	size_t x = 0, rgh;
+
+	if (array == NULL)
+		return (-1);
+
+	if (array[0] != value)
+	{
+		for (x = 1; x < size && array[x] <= value; x = x * 2)
+			printf("Value checked array[%ld] = [%d]\n", x, array[x]);
+	}
+	rgh = x < size ? x : size - 1;
+	printf("Value found between indexes [%ld] and [%ld]\n", x / 2, rgh);
+	return (_binary_search(array, x / 2, rgh, value));
+}
---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,11 @@
+#include "search_algos.h"
+
+/**
+ * rec_search - searches for a value in a sorted array of integers
+ * @array: is a pointer to the first element of the array to search
+ * @size: is the number of elements in array
+ * @value: is the value to search for
+ * Return: If value is not present in array or if array is NULL
+ * your function must return -1
+ */
+
