94a89627b83d1a280b88792afc6a51deb002d591
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-10-10 14:53:37 -0700

    advanced tasks

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -2,9 +2,9 @@
 
 /**
  * jump_search - searches for a value in a sorted array of integers
- * @array is a pointer to the first element of the array to search in
- * @size is the number of elements in array
- * @value is the value to search
+ * @array: is a pointer to the first element of the array to search in
+ * @size: is the number of elements in array
+ * @value: is the value to search
  * Return: value is not present in array or if array is NULL
  * your function must return -1
  */
